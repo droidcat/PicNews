6501c4243eeb3bdaa5eb5c1cdacb1dca704a6fb0
droidcat <droidroidcat@gmail.com>
2015-09-11 01:43:49 +0800

    常量类

---
+++ app/src/main/java/com/droidroid/imageloaderdemo/Constants.java
@@ -0,0 +1,15 @@
+package com.droidroid.imageloaderdemo;
+
+/**
+ * Created by Droidroid on 2015/9/11.
+ */
+public class Constants {
+
+    public static final String DB_NAME = "pic_news";
+
+    public static final int DB_VERSION = 2;
+
+    public static final String FIRST_NEWS_ITEM_URL = "http://api.3g.ifeng.com/get_pic_list?channel=news&pagesize=1&pageindex=0";
+
+
+}
