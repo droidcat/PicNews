90f23aa7787fbe448e36839f561b038c96d995bc
droidcat <droidroidcat@gmail.com>
2015-09-13 01:42:33 +0800

    添加常量值

--- app/src/main/java/com/droidroid/imageloaderdemo/Constants.java
+++ app/src/main/java/com/droidroid/imageloaderdemo/Constants.java
@@ -9,7 +9,8 @@ public class Constants {
 
     public static final int DB_VERSION = 2;
 
-    public static final String FIRST_NEWS_ITEM_URL = "http://api.3g.ifeng.com/get_pic_list?channel=news&pagesize=1&pageindex=0";
+    public static final String SINGLE_NEWS_URL = "http://api.3g.ifeng.com/get_pic_list?channel=news&pagesize=1&pageindex=0";
 
+    public static final String FIFTEEN_NEWSES_URL = "http://api.3g.ifeng.com/get_pic_list?channel=news&pagesize=15&pageindex=0";
 
 }
